2e191233ba81b9efd4a6f86c3cab5f497fa07ba4
Zuzky <sleepyzuzki@gmail.com>
2024-12-29 11:24:45 -0500

    [CHANGE]: Added images width and height

--- src/components/projects_component/project/project.jsx
+++ src/components/projects_component/project/project.jsx
@@ -1,6 +1,6 @@
 export default function Project() {
   return <wa-card with-image with-footer className="card-overview">
-    <img slot='image' src='https://placehold.co/300x200' alt='some project'/>
+    <img slot='image' src='https://placehold.co/300x200' alt='some project' width='300px' height='200px' />
     <p>Descripcion del projecto</p>
 
     <div slot='footer'>
