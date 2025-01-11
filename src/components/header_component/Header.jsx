49a63a50c0fd538a7250fbe6066ad2659a2cd3a2
Zuzky <sleepyzuzki@gmail.com>
2025-01-11 13:21:14 -0500

    Add shadow to header navbar section
    Added a `shadow-sm` class to enhance the visual separation of the header's navbar. This improves readability and provides a subtle depth effect for better UI aesthetics.

--- src/components/header_component/Header.jsx
+++ src/components/header_component/Header.jsx
@@ -2,7 +2,7 @@ import Link from "../link_component/Link.jsx";
 
 export default function Header() {
   return <header slot='header' className='wa-body-s'>
-    <section id='navbar' className="wa-split w-full flex flex-col items-center lg:flex-row">
+    <section id='navbar' className="wa-split w-full flex flex-col items-center lg:flex-row shadow-sm">
       <div>
         <wa-icon family='duotone' name="code" class="text-4xl align-middle"></wa-icon>
         <h1 id="brand-name" className="wa-heading-s wa-desktop-only inline-block align-middle text-2xl font-bold">
