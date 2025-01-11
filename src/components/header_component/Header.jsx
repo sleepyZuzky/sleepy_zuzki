8b0574633e6c4737ce8c864f28e63b3bda340d87
Zuzky <sleepyzuzki@gmail.com>
2025-01-11 16:37:59 -0500

    refactor div tag

--- src/components/header_component/Header.jsx
+++ src/components/header_component/Header.jsx
@@ -3,12 +3,12 @@ import Link from "../link_component/Link.jsx";
 export default function Header() {
   return <header slot='header' className='wa-body-s shadow-sm'>
     <section id='navbar' className="wa-split w-full flex flex-col items-center lg:flex-row">
-      <div>
+      <a href='/' className='wa-split-item'>
         <wa-icon family='duotone' name="code" class="text-4xl align-middle"></wa-icon>
         <h1 id="brand-name" className="wa-heading-s wa-desktop-only inline-block align-middle text-2xl font-bold">
           Sleepy Zuzki
         </h1>
-      </div>
+      </a>
 
       <div className="text-sm md:text-lg">
         <Link name={'Sobre mi'} refName={'#avatar'}/>
